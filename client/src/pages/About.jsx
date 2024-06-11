a57d2a4062cf795583cfaca47d6c17ffeeda24e6
dammar093 <dammarrana093@gmail.com>
2024-06-11 20:40:21 +0545

    add about page

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AboutComponent from '../components/AboutComponent'
+
+const About = () => {
+  return (
+    <div className='mt-[80px]'>
+      <AboutComponent />
+    </div>
+  )
+}
+
+export default About
