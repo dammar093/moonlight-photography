560980b0aadc42af763db11134c65aea5488e325
dammar093 <dammarrana093@gmail.com>
2024-06-11 20:41:12 +0545

    add service page

---
+++ client/src/pages/Service.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ServiceComponent from '../components/ServiceComponent'
+
+export const Service = () => {
+  return (
+    <div className='mt-[80px]'>
+      <ServiceComponent />
+    </div>
+  )
+}
