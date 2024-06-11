963300ae82bdf3c9a19faf34d41f1feeea91b356
dammar093 <dammarrana093@gmail.com>
2024-06-11 20:40:39 +0545

    add home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection'
+import WelcomeComponen from '../components/WelcomeComponen'
+import ServiceComponent from '../components/ServiceComponent'
+import Tool from '../components/Tool'
+import GalleryComponent from '../components/GalleryComponent'
+import PricingComponent from '../components/PricingComponent'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='mt-[70px] '>
+      <HeroSection />
+      <WelcomeComponen />
+      <ServiceComponent />
+      <Tool />
+      <PricingComponent />
+      <GalleryComponent isAll={true} />
+    </div>
   )
 }
 
