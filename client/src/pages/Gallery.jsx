262adb019afbac590eb40b91c8446702b98e51c1
dammar093 <dammarrana093@gmail.com>
2024-06-11 20:40:28 +0545

    add gallery page

---
+++ client/src/pages/Gallery.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GalleryComponent from '../components/GalleryComponent'
+
+const Gallery = () => {
+  return (
+    <div className='mt-[80px]'>
+      <GalleryComponent isAll={false} />
+    </div>
+  )
+}
+
+export default Gallery
