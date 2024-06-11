0f1d6d8f05f339ef23dc50168c9e4f3fe89d828a
dammar093 <dammarrana093@gmail.com>
2024-06-11 20:41:22 +0545

    add routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,10 @@ import App from './App.jsx'
 import Home from './pages/Home.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
+import About from './pages/About.jsx'
+import Pricing from './pages/Pricing.jsx'
+import { Service } from './pages/Service.jsx'
+import Gallery from './pages/Gallery.jsx'
 const router = createBrowserRouter(
   [
     {
@@ -14,7 +18,22 @@ const router = createBrowserRouter(
           path: "/",
           element: <Home />
         },
-
+        {
+          path: '/about',
+          element: <About />
+        },
+        {
+          path: '/pricing',
+          element: <Pricing />
+        },
+        {
+          path: '/services',
+          element: <Service />
+        },
+        {
+          path: 'gallery',
+          element: <Gallery />
+        }
       ]
     }
   ]
