cf86c847331b347b015301f7f791af432f55dd67
dammar093 <dammarrana093@gmail.com>
2024-06-11 09:08:50 +0545

    add tailwind css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Home from './pages/Home.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import './index.css'
 const router = createBrowserRouter(
   [
     {
