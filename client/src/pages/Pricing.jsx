a91393c5519a2a271e32bc8a1a8b24e96578fade
dammar093 <dammarrana093@gmail.com>
2024-06-11 20:41:02 +0545

    add pricing page

---
+++ client/src/pages/Pricing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PricingComponent from '../components/PricingComponent'
+
+const Pricing = () => {
+  return (
+    <div className='mt-[80px]'>
+      <PricingComponent />
+    </div>
+  )
+}
+
+export default Pricing
