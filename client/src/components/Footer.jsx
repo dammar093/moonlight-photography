8dbac591bac416641d4a03de3c86518247d2b8fc
dammar093 <dammarrana093@gmail.com>
2024-06-12 09:13:53 +0545

    chage text color

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -64,8 +64,8 @@ const Footer = () => {
             </div>
           </div>
         </Container>
-        <div className='text-center py-2 text-white border-t text-sm md:text-md'>
-          Copyright &copy; {new Date().getFullYear()} All-right reserved by Moonlight Photography powered by <Link to={'https://www.facebook.com/dammarrana093'}>Dammar Singh Rana</Link>
+        <div className='text-center py-2 text-white border-t text-[12px] md:text-md'>
+          Copyright &copy; {new Date().getFullYear()} All-right reserved by Moonlight Photography powered by <Link className='text-blue-600' to={'https://www.facebook.com/dammarrana093'}>Dammar Singh Rana</Link>
         </div>
       </footer>
     </section>
