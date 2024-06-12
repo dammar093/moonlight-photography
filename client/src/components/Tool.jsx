97ad37e5ad7c64ae68e07c99059d732a2bfebd90
dammar093 <dammarrana093@gmail.com>
2024-06-12 06:44:28 +0545

    add scrolltrigger on tool component

--- client/src/components/Tool.jsx
+++ client/src/components/Tool.jsx
@@ -1,5 +1,10 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Container from './Container'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(useGSAP)
+gsap.registerPlugin(ScrollTrigger);
 const tools = [
   {
     id: 1,
@@ -27,20 +32,68 @@ const tools = [
   }
 ]
 const Tool = () => {
+  const containerRef = useRef(null)
+  const scrollRef = useRef(null)
+  useGSAP(
+    () => {
+      const boxes = gsap.utils.toArray(containerRef.current.children);
+      boxes.forEach((box, index) => {
+        gsap.from(box, {
+          y: 150,
+          opacity: 0,
+          stagger: 0.2,
+          delay: index * 0.06,
+          scrollTrigger: {
+            trigger: box,
+            start: "-50 70%",
+            end: "top 20%",
+          },
+          ease: "power1.inOut",
+
+        });
+
+      });
+    },
+    { scope: containerRef }
+  );
+  useGSAP(
+    () => {
+      const boxes = gsap.utils.toArray(scrollRef.current.children);
+      boxes.forEach((box, index) => {
+        gsap.from(box, {
+          y: 150,
+          opacity: 0,
+          stagger: 0.2,
+          delay: index * 0.06,
+          scrollTrigger: {
+            trigger: box,
+            start: "-50 70%",
+            end: "top 20%",
+          },
+          ease: "power1.inOut",
+
+        });
+
+      });
+    },
+    { scope: scrollRef }
+  );
   return (
     <section>
       <Container>
         <div className='w-full my-4' id='service'>
           <div>
-            <div className='my-8'>
+            <div className='my-8' ref={containerRef}>
               <h2 className='capitalize text-[#f3f3f3] font-bold text-[25px] text-center md:text-[30px]'>Our Tools</h2>
               <div className='h-1 w-[100px] rounded bg-orange-500 mx-auto'></div>
             </div>
-            <div className='grid grid-cols-1 gap-3 md:grid-cols-3 items-center w-full place-content-center place-items-center'>
+            <div className='grid grid-cols-1 gap-3 md:grid-cols-3 items-center w-full place-content-center place-items-center' ref={scrollRef}>
               {
                 tools.map((tool) => (
-                  <div className='h-[250px] w-full md:w-[31vw]  bg-[#f3f3f3] rounded service hover:opacity-[0.7] transition-all ease-linear delay-100' key={tool.id}>
-                    <img className="w-full h-full  aspect-video" src={tool.image} alt="" />
+                  <div key={tool.id}>
+                    <div className='h-[250px] w-full md:w-[31vw]  bg-[#f3f3f3] rounded service hover:opacity-[0.7] transition-all ease-linear delay-100' >
+                      <img className="w-full h-full  aspect-video" src={tool.image} alt="" />
+                    </div>
                   </div>
                 ))
               }
