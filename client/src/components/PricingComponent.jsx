81c9764fc00756e615f456a70e5dad8d4848cde5
dammar093 <dammarrana093@gmail.com>
2024-06-12 06:37:52 +0545

    add scroll trigger on pricing component

--- client/src/components/PricingComponent.jsx
+++ client/src/components/PricingComponent.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Container from './Container'
-
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(useGSAP)
+gsap.registerPlugin(ScrollTrigger);
 const prices = [
   {
     id: 1,
@@ -30,43 +34,91 @@ const prices = [
   }
 ]
 const PricingComponent = () => {
+  const containerRef = useRef(null)
+  const scrollRef = useRef(null)
+  useGSAP(
+    () => {
+      const boxes = gsap.utils.toArray(containerRef.current.children);
+      boxes.forEach((box, index) => {
+        gsap.from(box, {
+          y: 150,
+          opacity: 0,
+          stagger: 0.2,
+          delay: index * 0.06,
+          scrollTrigger: {
+            trigger: box,
+            start: "-50 70%",
+            end: "top 20%",
+          },
+          ease: "power1.inOut",
+
+        });
+
+      });
+    },
+    { scope: containerRef }
+  );
+  useGSAP(
+    () => {
+      const boxes = gsap.utils.toArray(scrollRef.current.children);
+      boxes.forEach((box, index) => {
+        gsap.from(box, {
+          y: 150,
+          opacity: 0,
+          stagger: 0.2,
+          delay: index * 0.06,
+          scrollTrigger: {
+            trigger: box,
+            start: "-50 70%",
+            end: "top 20%",
+          },
+          ease: "power1.inOut",
+
+        });
+
+      });
+    },
+    { scope: scrollRef }
+  );
   return (
     <section>
       <Container>
         <div className='w-full my-4' id='service'>
           <div>
-            <div className='my-8'>
+            <div className='my-8' ref={containerRef}>
               <h2 className='capitalize text-[#f3f3f3] font-bold text-[25px] text-center md:text-[30px]'>our Pricing</h2>
               <div className='h-1 w-[100px] rounded bg-orange-500 mx-auto'></div>
             </div>
-            <div className='grid grid-cols-1 gap-3 md:grid-cols-3 items-center w-full place-content-center place-items-center'>
+            <div className='grid grid-cols-1 gap-3 md:grid-cols-3 items-center w-full place-content-center place-items-center' ref={scrollRef}>
               {
                 prices.map((price) => (
-                  <div className='w-full md:w-[31vw]  bg-[#f4f4f3] rounded p-8' key={price.id}>
-                    <div className='my-3 text-3xl text-center font-extrabold text-slate-800'>
-                      NRP.{price.price}/{price.duration}
-                    </div>
-                    <div className='capitalize text-2xl font-bold text-slate-800'>
-                      {
-                        price.title
-                      }
-                    </div>
-                    <div className='text-lg text-justify my-2 font-semibold text-slate-800'>
-                      {
-                        price.desc
-                      }
-                    </div>
-                    <div className='px-4'>
-                      <ul className='text-lg font-semibold list-disc text-slate-800'>
+                  <div key={price.id} >
+                    <div className='w-full md:w-[31vw]  bg-[#f4f4f3] rounded p-8' >
+                      <div className='my-3 text-3xl text-center font-extrabold text-slate-800'>
+                        NRP.{price.price}/{price.duration}
+                      </div>
+                      <div className='capitalize text-2xl font-bold text-slate-800'>
                         {
-                          price.services.map(service => (
-                            <li key={service}>{service}</li>
-                          ))
+                          price.title
                         }
-                      </ul>
-                    </div>
-                    <div className='w-1/2 mx-auto mt-4'>
-                      <button className='text-orange-500 border-2 border-solid border-orange-500 hover:bg-orange-500 hover:text-slate-800 px-6 py-2 font-semibold transition-all ease-linear delay-100'>Book Now</button>
+                      </div>
+                      <div className='text-lg text-justify my-2 font-semibold text-slate-800'>
+                        {
+                          price.desc
+                        }
+                      </div>
+                      <div className='px-4'>
+                        <ul className='text-lg font-semibold list-disc text-slate-800'>
+                          {
+                            price.services.map(service => (
+                              <li key={service}>{service}</li>
+                            ))
+                          }
+                        </ul>
+                      </div>
+                      <div className='w-1/2 mx-auto mt-4'>
+                        <button className='text-orange-500 border-2 border-solid border-orange-500 hover:bg-orange-500 hover:text-slate-800 px-6 py-2 font-semibold transition-all ease-linear delay-100'>Book Now</button>
+                      </div>
                     </div>
                   </div>
                 ))
